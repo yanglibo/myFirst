e524b9c1d831faca25258bbe72c1600170c86da2
yanglibo <a51764579@qq.com>
2015-08-01 10:41:41 +0800

    幅度萨菲

---
+++ javascript/chapter3/global/globalObject.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Administrator on 15-8-1.
+ */
+console.log(Math.round(12.3));
+x=0;
+console.log(Boolean(!!x));
+var s = "fdasfdsa";
+console.log(s.toUpperCase())
+console.log(s)
+var s1 = 1-"x";
+console.log(s1);
+console.log(1-[2]);
+console.log(typeof false)
+console.log(/\d/.toString())
