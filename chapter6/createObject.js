145eee73f1f628a0446fb912c0eaeb46fce2ffb3
yanglibo <yanglibo@21tb.com>
2015-08-11 11:59:52 +0800

    创建对象的三种样式

---
+++ chapter6/createObject.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Administrator on 2015/8/11.
+ */
+console.log("---直接量========")
+var f ={
+    "1":"12"
+}
+console.log(f["1"]);
+console.log("---new========")
+var f1 = new Object();
+f1["1"]="12";
+
+console.log(f["1"]);
+
+console.log("---inherit========")
+function inherit(o){
+    if(Object.create){
+        console.log("使用了Object.create")
+        return Object.create(o);
+    }
+    function f(){
+
+    }
+    f.prototype = o;
+    return new f();
+}
+
+var f2 = inherit(Object.prototype);
+f2["1"]="12";
+
+console.log(f["1"]);
